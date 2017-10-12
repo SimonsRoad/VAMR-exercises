b07341a6001a2fda8b036d113629ef2e28691c84
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-10-12 09:21:21 +0200

    started implementing distortion correction, not working yet

--- exercise01/undistort_image.m
+++ exercise01/undistort_image.m
@@ -9,28 +9,31 @@ function  img_u = undistort_image(img_d, D)
 % Samuel Nyffenegger, 10.10.17
     
 %%  calculations
+img_d = data.img_0001;
+D = data.D;
 
-% loop over points
-for i = 1:N
+clc
 
-    % 
-    pc = #
-    
-    % map into image plane with normalized coordinates
-    p = [p_c(1)/p_c(3); p_c(2)/p_c(3)];
+img_u = img_d;
+[Wy, Wx] = size(img_d); 
 
-    % apply lens distortion
-    r = sqrt(p(1).^2 + p(2).^2);
-    p_d = (1 + D(1)*r.^2 + D(2)*r.^4) * p; 
-    p_d_tilde = [p_d;1];
-    p_tilde = K*p_d_tilde; 
-
-    % get discretized pixel coordinates 
-    u = p_tilde(1)/p_tilde(3);
-    v = p_tilde(2)/p_tilde(3);
-
-    % update
-    points(i,:) = [u, v];
+% loop over pixels
+for x = 1:Wx
+    for y = 1:Wy
+        % generate normalized distorted pixel coordinate 
+        p_d = [x/Wx; y/Wy];
+        
+        % correct for lens distorsion
+        r = sqrt(p_d(1).^2 + p_d(2).^2);
+        p = 1/(1 + D(1)*r.^2 + D(2)*r.^4) * p_d;
+        
+        % generate normalized distorted pixel coordinate 
+        p_u = [round(Wx*p(1)); round(Wy*p(2))];
+        img_u(p_u(2),p_u(1)) = data.img_0001(y,x);
+        
+        % insert into undistorted image
+        
+    end
 end
 
 
