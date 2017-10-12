190eefb75b70fa4ffeab46f7bd4b0d2c7f7af191
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-10-12 16:00:19 +0200

    clean up

--- exercise01/undistort_image.m
+++ exercise01/undistort_image.m
@@ -31,8 +31,8 @@ for u = 1:Wx
         % distorted homogeneous Discretizised pixel coordinated (u_d, v_d)
         p_D_d = K*p_N_d;
              
-        % closest neighbour approach, bounded "into" picture
-        u_d = round(p_D_d(1)/p_D_d(3));
+        % closest neighbour approach
+        u_d = round(p_D_d(1)/p_D_d(3)); 
         v_d = round(p_D_d(2)/p_D_d(3)); 
         
         % update through backward warping
@@ -41,36 +41,5 @@ for u = 1:Wx
     end
 end
 
-
-
-
 end
-
-
-
-%         % distorted homogeneous discretized pixel coordinates (u_d,v_d)
-%         pc_d = [u_d; v_d; 1];
-%         
-%         % map into image plane with normalized coordinates (x_d, y_d)
-%         p0_d = inv(K)*pc_d;
-%         x_d = p0_d(1)/p0_d(3);
-%         y_d = p0_d(2)/p0_d(3);
-%                 
-%         % correct for lens distorsion
-%         r = sqrt(x_d.^2 + y_d.^2);
-%         p0 = ones(3,1);
-%         p0(1:2) = 1/(1 + D(1)*r.^2 + D(2)*r.^4) * p0_d(1:2); 
-%              
-%         % get discretized pixel coordinates 
-%         pC = K*p0; 
-%         u = round(pC(1)/pC(3));
-%         v = round(pC(2)/pC(3));
-%         
-%         % fprintf(['u_d=',num2str(u_d),', v_d=',num2str(v_d),' u=',num2str(u),', v=',num2str(v),', r=',num2str(r),'\n'])
-%         
-%         % update rounded distorted pixel coordinate 
-%         if 0<u && u<=Wx && 0<v && v<=Wy
-%             img_u(round(v),round(u)) = img_d(v_d,u_d);
-%         end
-%                
 
