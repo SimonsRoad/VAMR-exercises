c12254b8a2688bc6fa2939a8d96b6a1e5340f615
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-10-16 16:08:29 +0200

    basic structure prepared

---
+++ exercise02/code/main.m
@@ -0,0 +1,16 @@
+%   Vision Algorithms for Mobile Robots
+%   Exercise 02
+%   The Perspective-n-Point (PnP) problem
+%   Samuel Nyffenegger
+%   16.10.2017
+
+%%  calculations
+
+% init and load
+clear all; close all; clc;
+init('VAMR/exercise02');
+data = loadData();
+
+
+
+
---
+++ exercise02/code/param.m
@@ -0,0 +1,13 @@
+% parameter file
+% Samuel Nyffenegger, 16.10.17
+
+classdef param
+   properties (Constant)
+        % control parameters 
+        dockFig = true;             % dock all figures
+        
+        % parameters
+        n_reference_points = 12;    % number of reference points
+        m_images = 749;             % number of images
+   end
+end
