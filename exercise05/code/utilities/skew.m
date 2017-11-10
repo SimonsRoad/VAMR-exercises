3185851efdf750a3498f41a0505ceed3727f9989
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-11-10 17:40:04 +0100

    triangulation works, colors in CW added

---
+++ exercise05/code/utilities/skew.m
@@ -0,0 +1,9 @@
+function mat = skew(vec)
+    %% returns a matrix, sd a crossproduct is calculated using matrix multiplication
+    % [vec]_x
+    
+    mat=[0      -vec(3) vec(2) ; ... 
+         vec(3) 0       -vec(1) ; ... 
+         -vec(2) vec(1)  0 ];
+
+end
