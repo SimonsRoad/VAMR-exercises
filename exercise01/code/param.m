5f13019201c1bb7dff449766f4e1a2788787d7f9
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-11-21 12:31:20 +0100

    cube coordinates bug fixed

--- exercise01/code/param.m
+++ exercise01/code/param.m
@@ -4,9 +4,9 @@
 classdef param
    properties (Constant)
         % control parameters 
-        dockFig = true;     % dock all figures
+        dockFig = false;     % dock all figures
         cube_x = 2;         % x position [units] of top left corner
-        cube_y = 2;         % y position [units] of top left corner
+        cube_y = 3;         % y position [units] of top left corner
         cube_a = 2;         % length of cube [units]
         n_frames = 10; %736;       % number of video frames, -1 to inherit 
         
