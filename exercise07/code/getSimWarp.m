cb2a88f8947b2229ff92fd9449094e10aa0c7696
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-12-04 14:10:10 +0100

    part 1 works

--- exercise07/code/getSimWarp.m
+++ exercise07/code/getSimWarp.m
@@ -1,2 +1,16 @@
 function W = getSimWarp(dx, dy, alpha_deg, lambda)
-% alpha given in degrees, as indicated
+% gives affine transformation matrix
+% Input:
+%   dx, translation in x direction
+%   dy, translation in y direction
+%   alpha, degrees, rotaion arrount positive z-axis
+%   lambda, scaling 
+% Output:
+%   W, 2x3 affine transformation matrix
+
+%% calculations
+
+W = [lambda.*[cos(deg2rad(alpha_deg)), - sin(deg2rad(alpha_deg)) ; ...
+    sin(deg2rad(alpha_deg)), cos(deg2rad(alpha_deg))], [dx; dy]];
+
+end
